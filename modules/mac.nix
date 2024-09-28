1df1f650dbc938561405c0b00cfe3b996156a364
larstvei <larstvei@ifi.uio.no>
2024-09-28 17:09:24 +0200

    Forgot to add nixbld (but unsure if necessary)

--- modules/mac.nix
+++ modules/mac.nix
@@ -10,6 +10,7 @@
       system = "aarch64-darwin";
       extra-platforms = [ "aarch64-darwin" "x86_64-darwin" ];
       experimental-features = [ "nix-command" "flakes" ];
+      build-users-group = "nixbld";
       trusted-users = [ "root" "larstvei" "@admin" ];
     };
   };
