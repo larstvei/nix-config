205f731bd1fb4c8810857865fdb2a66fab7ce4db
larstvei <larstvei@ifi.uio.no>
2025-08-23 15:57:51 +0200

    Move homebrew to host

--- system/darwin/default.nix
+++ system/darwin/default.nix
@@ -51,35 +51,4 @@
       };
     };
   };
-
-  homebrew = {
-    enable = true;
-    onActivation = {
-      # autoUpdate = true;
-      upgrade = true;
-      cleanup = "uninstall";
-    };
-    casks = [
-      "amethyst"
-      "arc"
-      "chatgpt"
-      "darktable"
-      "dropbox"
-      "google-chrome"
-      "iina"
-      "karabiner-elements"
-      "obs"
-      "ollama"
-      "processing"
-      "racket"
-      "raycast"
-      "remarkable"
-      "signal"
-      "skim"
-      "supercollider"
-      "utm"
-      "zoom"
-      "zen-browser"
-    ];
-  };
 }
