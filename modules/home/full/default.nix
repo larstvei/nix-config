3d6ed3ac9cee02a7824c61a87c9dc2ae9033aff5
larstvei <larstvei@ifi.uio.no>
2025-09-13 10:55:02 +0200

    Make minimal/small/medium/full configs

---
+++ modules/home/full/default.nix
@@ -0,0 +1,7 @@
+{
+  imports = [
+    ../.
+    ../../languages
+    ../../tools
+  ];
+}
---
+++ modules/home/medium/default.nix
@@ -0,0 +1,13 @@
+{
+  imports = [
+    ../.
+    ../../languages/c
+    ../../languages/clojure
+    ../../languages/nix
+    ../../languages/python
+    ../../tools/editor
+    ../../tools/git
+    ../../tools/shell
+    ../../tools/utils
+  ];
+}
