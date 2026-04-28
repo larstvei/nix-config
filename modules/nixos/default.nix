ba901510c4030bcaedb9f903c93ca5fbca09021b
larstvei <larstvei@ifi.uio.no>
2026-04-28 08:58:28 +0200

    Enable jack

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -53,6 +53,7 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    jack.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
