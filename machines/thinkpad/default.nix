fa5deef737909ce051a73e972612077bb2680701
larstvei <larstvei@ifi.uio.no>
2026-06-10 01:22:29 +0200

    Let the thinkpad lid logo LED do its thing

--- machines/thinkpad/default.nix
+++ machines/thinkpad/default.nix
@@ -16,29 +16,6 @@
 
   services.gnome.gnome-keyring.enable = true;
 
-  systemd.services.disable-thinkpad-leds = {
-    description = "Disable ThinkPad lid logo LED and power button led";
-    wantedBy = [
-      "basic.target"
-      "suspend.target"
-    ];
-    after = [
-      "hibernate.target"
-      "hybrid-sleep.target"
-      "suspend-then-hibernate.target"
-      "suspend.target"
-      "sysinit.target"
-    ];
-
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = [
-        "/bin/sh -c 'echo 0 > /sys/class/leds/tpacpi::power/brightness'"
-        "/bin/sh -c 'echo 0 > /sys/class/leds/tpacpi::lid_logo_dot/brightness'"
-      ];
-    };
-  };
-
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
